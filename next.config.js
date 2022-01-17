2c55029fab714f52cfe2222c70c5e5a19aa04f8f
Ivan Chen <IvanChen07@gmail.com>
2022-01-16 23:12:29 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: '/conway/',
+  assetPrefix: '/conway-game-of-life/',
 }
 
 module.exports = nextConfig

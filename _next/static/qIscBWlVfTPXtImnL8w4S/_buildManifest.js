bfaca8ec2e0ce501070e5dbada8edc7eb2a626a6
anivanchen <IvanChen07@gmail.com>
2022-01-17 04:59:31 +0000

    Deploying to gh-pages from @ anivanchen/conway-game-of-life@9ecf1b8a556e5ac6cb988e8ed0305738fbb07055 🚀

---
+++ _next/static/qIscBWlVfTPXtImnL8w4S/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/69a56966165699d6.css","static/chunks/pages/index-d5fd866f9438c16b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
